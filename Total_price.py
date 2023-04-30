aca5a3ad113dcebcb32910161bc9e02f9672ed9e
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-04-30 15:32:46 +0530

    Update Total_price.py

--- Total_price.py
+++ Total_price.py
@@ -1,3 +1,13 @@
+#Question 1 - 
+#Description - Create a small command-line program in Python to calculate the total invoice amount for the below purchases - 
+#Book - Introduction to Python Programming : Rs 499.00
+#Book - Python Libraries Cookbook : Rs. 855.00
+#Book - Data Science in Python : Rs. 645.00
+#Taxes and additional charges are described as details - 
+#GST : 12%
+#Delivery Charges : Rs. 250.00
+
+
 x=float(input("Enter no. of 'Introduction to python programming' books:"))*499
 y=float(input("Enter no. of 'Python libraries Cookbook' books:"))*855
 z=float(input("Enter no. of 'Data Science in Python' books:"))*645
