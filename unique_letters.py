4c6440c624853759ed7eb63c676e2efaa0fe9f81
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-04-30 15:34:50 +0530

    Update unique_letters.py

--- unique_letters.py
+++ unique_letters.py
@@ -1,3 +1,10 @@
+#Question 2 - 
+#Description: Write a program in Python to print the number of unique letters in a string. Only new letters from the string should be counted and not duplicates.
+#Input : string1 = "India"
+#Output : uniqueLetters = i,n,d,a
+#Input : string1 = "Dedication"
+#Output : uniqueLetters = d,e,i,c,a,t,o,n
+
 
 string1 = input()
 uniqueLetters = ""
